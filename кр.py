9ca656f49c4fc2964b13080f2a5e3bbac099de57
gorgandiana <31985726+gorgandiana@users.noreply.github.com>
2017-12-07 11:58:20 +0300

    Add files via upload

---
+++ кр.py
@@ -0,0 +1,24 @@
+word_count = 0
+lines = []
+cell = []
+words = []
+
+with open('quotes.txt', encoding = 'utf-8') as f:
+    lines = f.readlines()
+for line in lines:
+    cells = line.split('—')
+    for cell in cells:
+        words = cell.split()
+        for word in words:
+            if len(cell)<=10 and len(word[0])<5:
+                print(line)
+
+
+with open('quotes.txt', encoding = 'utf-8') as f:   #не получиилось(((((((
+    lines = f.readlines()
+for line in lines:
+            for word in line:
+                if word == 'разум':
+                    print(line.count('разум'))    
+
+
