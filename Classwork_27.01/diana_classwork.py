05993ba54480f3293df0dfc97f2a40fe4e57fa0d
gorgandiana <31985726+gorgandiana@users.noreply.github.com>
2018-01-27 11:08:44 +0300

    Add files via upload

---
+++ Classwork_27.01/diana_classwork.py
@@ -0,0 +1,14 @@
+d = {2: 'He', 3: 'Li', 4: 'Be'}
+d[1] = 'H'
+
+print('Перебор ключей')
+for key in d:
+    print(key, d[key])
+keys = list(d.keys())
+print('Перебор значений')
+for value in d.values():
+    print(value)
+values = list(d.values())
+print('Перебор пар')
+for key, value in d.items():
+    print(key, value)
