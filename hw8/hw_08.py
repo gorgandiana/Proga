2452df7f939caeffe7668130af0bc2b841c8891f
gorgandiana <31985726+gorgandiana@users.noreply.github.com>
2018-02-18 22:44:30 +0300

    Add files via upload

---
+++ hw8/hw_08.py
@@ -0,0 +1,26 @@
+import random
+
+def hint():
+    d = {}
+    with open("words_hints1.txt", 'r', encoding='utf-8') as f:
+        for line in f:
+             word, hint = line.strip().split(" ", maxsplit=1)
+             d[word] = hint
+    return d
+
+
+def programm(dictionary):
+    tries = 0
+    key = random.choice(list(dictionary))
+    print("Привет!Поиграем? Я буду загадывать слова, а ты, используя подсказки, будешь их угадывать:)\n\
+Подсказка:", dictionary[key]," ...")
+    attemp = input("Загаданное слово: ")
+    while attemp.lower() != key :      
+        print("Ответ неправильный")
+        attemp = input("Загаданное слово: ")
+        tries += 1
+    if attemp.lower() == key:
+        print("Правильный ответ!")    
+
+
+programm(hint())
