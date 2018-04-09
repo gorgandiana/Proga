fbbf6dea0551154fcf57bb3d1aeca9d79ba1b6fb
gorgandiana <31985726+gorgandiana@users.noreply.github.com>
2018-04-09 21:01:36 +0300

    Add files via upload

---
+++ hw_11/hw_11_changed.py
@@ -0,0 +1,13 @@
+
+import re
+def openfile():
+    with open ("Птицы - Класс птицы - Виды птиц.html",encoding = "utf-8") as f:
+        text = f.read()
+    result = re.sub('Птиц', 'Рыб', result) 
+    result = re.sub('птиц', 'рыб', result) 
+    result = re.sub('Птицей', 'Рыбой', result) 
+    result = re.sub('птицей', 'рыбой',result)
+   
+    with open ("text.txt","w", encoding = "utf-8") as t:
+        t.write(result)
+#с функциями ничего не работает:(
